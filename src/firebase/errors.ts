860092d4e2cbe52fe3dfc7d736bc811e43945427
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2023-04-10 05:33:57 +0700

    Update errors.ts

--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -4,4 +4,5 @@ export const FIREBASE_ERRORS = {
 
     "Firebase: Error (auth/user-not-found).": "Invalid email or password",
     "Firebase: Error (auth/wrong-password).": "Invalid email or password",
+    'Firebase: Error (auth/popup-closed-by-user).' : ''
 };
